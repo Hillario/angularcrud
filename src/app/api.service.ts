98c92c4217cfdb5e1ac80c9ca42c103d9bc00ff3
Hillario <hillcheswiztyphoon@gmail.com>
2023-02-11 07:20:46 +0300

    URL_to_Server_Github

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,21 +10,21 @@ export class ApiService {
 
   postProduct(data:any)
   {
-    return this.http.post<any>('http://localhost:3000/productList/',data)
+    return this.http.post<any>('https://my-json-server.typicode.com/hillario/angularcrud/productList',data)
   }
 
   getProduct()
   {
-    return this.http.get<any>('http://localhost:3000/productList/')
+    return this.http.get<any>('https://my-json-server.typicode.com/hillario/angularcrud/productList')
   }
 
   putProduct(data:any,id:number)
   {
-    return this.http.put<any>('http://localhost:3000/productList/'+id,data)
+    return this.http.put<any>('https://my-json-server.typicode.com/hillario/angularcrud/productList'+id,data)
   }
 
   deleteProduct(id:number)
   {
-    return this.http.delete('http://localhost:3000/productList/'+id)
+    return this.http.delete('https://my-json-server.typicode.com/hillario/angularcrud/productList'+id)
   }
 }
